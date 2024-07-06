ca1979401c62bfbe31fd98ff62a69273a4fc6530
PaisanKjn <bank200074@gmail.com>
2024-07-06 10:09:19 +0700

    First Commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style = {{fontWeight: 'bold'}}>Open up App.tsx to start working on your app!</Text>
+      <Text> I love when I can sleep all day</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#9fa',
     alignItems: 'center',
     justifyContent: 'center',
   },
