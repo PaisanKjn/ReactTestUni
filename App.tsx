b7502117bf75388692265e3819de99d693718de8
PaisanKjn <bank200074@gmail.com>
2024-09-07 10:12:28 +0700

    Drawer

--- App.tsx
+++ App.tsx
@@ -1,43 +1,61 @@
+import 'react-native-gesture-handler' // needs to be at the top of App.tsx
 import { View } from "react-native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { HeaderButtonsProvider } from "react-navigation-header-buttons";
 import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
 import CreatePostScreen from "./screens/CreatePostScreen";
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
+const HomeStack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function HomeStackScreen(){
+
+  return(
+    <HomeStack.Navigator
+    initialRouteName="Home"
+    screenOptions={{
+      // Global config
+      // headerStyle: { backgroundColor: "#205a41" },
+      // headerTintColor: "white",
+      headerTitleStyle: { fontWeight: "bold" },
+    }}
+  >
+    <HomeStack.Screen
+      options={{
+        title: "Main",
+      }}
+      name="Home"
+      component={HomeScreen}
+    />
+    <HomeStack.Screen
+      options={{
+        title: "About Us",
+        headerStyle: { backgroundColor: "#205a41" },
+        headerTintColor: "white",
+      }}
+      name="About"
+      component={AboutScreen}
+    />
+  </HomeStack.Navigator>
+  )
+};
 const App = (): React.JSX.Element => {
-  const HomeStack = createNativeStackNavigator();
+
+
+
+
   return (
-    <NavigationContainer>
-      <HomeStack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle: { backgroundColor: "#205a41" },
-          headerTintColor: "white",
-          headerTitleStyle: { fontWeight: "bold" },
-          headerTitleAlign: "center",
-        }}
-      >
-        <HomeStack.Screen
-          options={{
-            title: "Main",
-          }}
-          name="Home"
-          component={HomeScreen}
-        />
-        <HomeStack.Screen
-          options={{ title: "About Us" }}
-          name="About"
-          component={AboutScreen}
-        />
-        <HomeStack.Screen
-          options={{ title: "Create Post" }}
-          name="CreatePost"
-          component={CreatePostScreen}
-        />
-      </HomeStack.Navigator>
-    </NavigationContainer>
+    <HeaderButtonsProvider stackType="native">
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name = "HomeStack" component={HomeStackScreen}/>
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </HeaderButtonsProvider>
   );
 };
 
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,59 +1,89 @@
-import { StyleSheet, Text, View , Button} from 'react-native'
-import React from 'react'
-import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import React from "react";
+import MaterialIcon from "react-native-vector-icons/MaterialIcons";
+import { useNavigation } from "@react-navigation/native";
+import { useLayoutEffect } from "react";
+import AppLogo from "../components/AppLogo";
+import {
+  HeaderButton,
+  HeaderButtons,
+  Item,
+} from "react-navigation-header-buttons";
 
-const HomeScreen = ({navigation, route}: any):React.JSX.Element => {
+const HomeScreen = (): React.JSX.Element => {
+  const navigation = useNavigation<any>(); // return the props so you don't have to type the navigation/route shit thing
+
+  const MaterialHeaderButton = (props: any) => (
+    // the `props` here come from <Item ... />
+    // you may access them and pass something else to `HeaderButton` if you like
+    <HeaderButton IconComponent={MaterialIcon} iconSize={23} {...props} />
+  );
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "หน้าหลัก",
+      headerTitle: () => <AppLogo />,
+      headerTitleAlign: "center",
+      headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
+          <Item
+            title="menu"
+            iconName="menu"
+            onPress={() => Alert.alert("Alert","Hello React Native")}
+          />
+        </HeaderButtons>
+      ), 
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
+          <Item
+            title="hasNoMeaning"
+            iconName="logout"
+            onPress={() => Alert.alert("Alert","Logout")}
+          />
+        </HeaderButtons>
+      ),
+    });
+  }, [navigation]);
+
   const goToAbout = () => {
-    navigation.navigate('About', {
-      companyName: '泰国人公司',
-      companyId: 420
+    navigation.navigate("About", {
+      companyName: "泰国人公司",
+      companyId: 420,
     });
   };
   const goToCreatePost = () => {
-    navigation.navigate('CreatePost');
-  }
+    navigation.navigate("CreatePost");
+  };
   return (
-    <View style = {styles.container}>
-      <MaterialIcon name = "home" size = {40} color = '#ec3faa'/>
-      <Text style= {styles.header}>HomeScreen</Text>
-      <Button
-      color={"#67a33b"}
-      title = "About Us"
-      onPress={goToAbout}/>
-
-      <View style = {styles.postContainer}>
-       <Button
-       color={"#67a33b"}
-      title = "Create POST"
-      onPress={goToCreatePost}/>
-      <Text style = {styles.postText}>Post: <Text style = {styles.postContent}>{route.params?.post}</Text> </Text>
-     
-       </View>
+    <View style={styles.container}>
+      <MaterialIcon name="home" size={40} color="#ec3faa" />
+      <Text style={styles.header}>HomeScreen</Text>
+      <Button color={"#67a33b"} title="About Us" onPress={goToAbout} />
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-      padding: 20,
-    },
-    header: {
-      fontSize: 24,
-      fontWeight: "bold",
-    },
-    postContainer: {
-      alignItems: "center",
-      justifyContent: "center",
-      marginTop: 50,
-    },
-    postText: {
-      margin: 10,
-      fontSize: 16,
-    },
-    postContent: {
-      color: "blue", // เปลี่ยนสีข้อความที่ถูกส่งกลับมา
-              fontWeight: "bold",
-    },
-  });
+  container: {
+    padding: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  postContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 50,
+  },
+  postText: {
+    margin: 10,
+    fontSize: 16,
+  },
+  postContent: {
+    color: "blue", // เปลี่ยนสีข้อความที่ถูกส่งกลับมา
+    fontWeight: "bold",
+  },
+});
