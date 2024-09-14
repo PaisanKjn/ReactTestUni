a7f7323864352f31f157937ffbbb6d70d2b56969
PaisanKjn <bank200074@gmail.com>
2024-09-14 09:37:41 +0700

    Services

--- screens/ProductScreen.tsx
+++ screens/ProductScreen.tsx
@@ -1,45 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useCallback } from "react";
+import { useLayoutEffect } from "react";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 import {
-    HeaderButton,
-    HeaderButtons,
-    Item,
-  } from "react-navigation-header-buttons";
+  HeaderButton,
+  HeaderButtons,
+  Item,
+} from "react-navigation-header-buttons";
+import { findAllProduct } from "../services/product-service";
 
-const ProductScreen = ():React.JSX.Element => {
-    const navigation = useNavigation<any>();
-    const MaterialHeaderButton = (props: any) => (
-        // the `props` here come from <Item ... />
-        // you may access them and pass something else to `HeaderButton` if you like
-        <HeaderButton IconComponent={MaterialIcon} iconSize={23} {...props} />
-      );
+const ProductScreen = (): React.JSX.Element => {
+  const navigation = useNavigation<any>();
+  const MaterialHeaderButton = (props: any) => (
+    // the `props` here come from <Item ... />
+    // you may access them and pass something else to `HeaderButton` if you like
+    <HeaderButton IconComponent={MaterialIcon} iconSize={23} {...props} />
+  );
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-          headerTitleAlign: "center",
-          headerLeft: () => (
-            <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
-              <Item
-                title="menu"
-                iconName="menu"
-                onPress={() => navigation.openDrawer()
-                }
-              />
-            </HeaderButtons>
-          )
-        });
-      }, [navigation]);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitleAlign: "center",
+      headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
+          <Item
+            title="menu"
+            iconName="menu"
+            onPress={() => navigation.openDrawer()}
+          />
+        </HeaderButtons>
+      ),
+    });
+  }, [navigation]);
+
+  const getProduct = async () => {
+    try {
+      const response = await findAllProduct();
+     
+      console.log(response.data.data);
+    } catch (error:any) {
+      console.log(error.message);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(()=> {
+      getProduct();
+    }, [])
+  );
 
   return (
     <View>
       <Text>ProductScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
